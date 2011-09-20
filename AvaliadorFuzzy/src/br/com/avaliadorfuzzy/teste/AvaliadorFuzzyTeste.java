bb6a1b360ec9ec64125553c250efeed0bfca3159
bernardo.filardi <bernardo.filardi@bernardofilardi>
2011-09-19 23:57:38 -0300

    Commit classe de testes

--- AvaliadorFuzzy/src/br/com/avaliadorfuzzy/teste/AvaliadorFuzzyTeste.java
+++ AvaliadorFuzzy/src/br/com/avaliadorfuzzy/teste/AvaliadorFuzzyTeste.java
@@ -1,12 +1,18 @@
 package br.com.avaliadorfuzzy.teste;
 
+import br.com.avaliadorfuzzy.controller.FuzzyController;
+
 public class AvaliadorFuzzyTeste {
 
 	/**
 	 * @param args
 	 */
 	public static void main(String[] args) {
-				
+		FuzzyController ctrl = new FuzzyController();
+		
+		Double valorAvaliacao = ctrl.getFuzzyClassification(180, 1);
+		
+		System.out.println("O valor da avaliação é: " + valorAvaliacao);
 	}
 
 }
